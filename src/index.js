4359a73667c2fb0289c724a767e2f582d8a55da8
carolinaf10 <carolferreira1097@gmail.com>
2024-10-13 14:12:01 +0100

    Weather Sun-Thur

--- src/index.js
+++ src/index.js
@@ -19,6 +19,49 @@ function displayWeather(response) {
   temperatureIcon.innerHTML = `<img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${iconCode}.png" alt="${description}" />`;
 }
 
+function displayForecast(dailyData) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = "";
+
+  for (let i = 0; i < 5; i++) {
+    let dayForecast = dailyData[i];
+    let dayDiv = document.createElement("div");
+    dayDiv.classList.add("weather-forecast-day");
+
+    let dateElement = document.createElement("div");
+    dateElement.classList.add("weather-forecast-date");
+    dateElement.innerHTML = formatDay(new Date(dayForecast.time * 1000));
+
+    let iconElement = document.createElement("img");
+    iconElement.src = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${dayForecast.condition.icon}.png`;
+    iconElement.classList.add("weather-forecast-icon");
+
+    let temperaturesElement = document.createElement("div");
+    temperaturesElement.classList.add("weather-forecast-temperatures");
+
+    let maxTempElement = document.createElement("div");
+    maxTempElement.classList.add("weather-forecast-temperature");
+    maxTempElement.innerHTML = `<strong>${Math.round(
+      dayForecast.temperature.maximum
+    )}º</strong>`;
+
+    let minTempElement = document.createElement("div");
+    minTempElement.classList.add("weather-forecast-temperature");
+    minTempElement.innerHTML = `${Math.round(
+      dayForecast.temperature.minimum
+    )}º`;
+
+    temperaturesElement.appendChild(maxTempElement);
+    temperaturesElement.appendChild(minTempElement);
+
+    dayDiv.appendChild(dateElement);
+    dayDiv.appendChild(iconElement);
+    dayDiv.appendChild(temperaturesElement);
+
+    forecastElement.appendChild(dayDiv);
+  }
+}
+
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
@@ -26,10 +69,17 @@ function search(event) {
 
   let apiKey = "484ae542b9ffa311f44109c1a4aoa2t3";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let forecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
 
   axios
     .get(apiUrl)
-    .then(displayWeather)
+    .then((response) => {
+      displayWeather(response);
+      return axios.get(forecastUrl);
+    })
+    .then((forecastResponse) => {
+      displayForecast(forecastResponse.data.daily);
+    })
     .catch(function (error) {
       alert("City not found. Please try again!");
     });
@@ -57,9 +107,13 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-
   let formattedDay = days[day];
   return `${formattedDay} ${hours}:${minutes}`;
+}
+
+function formatDay(date) {
+  let options = { weekday: "long" };
+  return new Intl.DateTimeFormat("en-US", options).format(date);
 }
 
 let searchForm = document.querySelector("#search-form");
